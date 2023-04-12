b709f2cd7debc14c7f873db242e4bdc2f12906d4
DUABDUL <130210976+DUABDUL@users.noreply.github.com>
2023-04-12 13:14:40 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,24 @@
+#!/usr/bin/env/bash
+function gessinggame{
+  local num_files=$(ls -1|wc-1)
+  local user_gess=-1
+  while [[$user_guess-ne
+  $sum_files]]
+  do
+  echo "Guess how many files are in the current directory."
+  read user_guess
+  if[[$user_guess -it$num_files]]
+  then
+  echo "Your guess was too low"
+  eilf[[$user_guess -gt
+  $num_files]]
+  then
+  echo "your guess was too high."
+  else
+  echo"congratulattions! Your
+  guess was correct!"
+  fi
+  done
+  }
+  guessinggame
 
